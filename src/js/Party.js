6883c7d6b523156eead43a8c3fd4c245dc4d9fa7
slash <slash@slashware.net>
2016-05-14 20:38:51 -0500

    Only change room if there's a corridor

--- src/js/Party.js
+++ src/js/Party.js
@@ -18,9 +18,27 @@ var Party = {
 		return this.level.getRoom(this.location);
 	},
 	move: function(dx, dy){
-		this.location.x += dx;
-		this.location.y += dy;
-		console.log(this.location);
+		var direction = this.getDirection(this.location, {x: this.location.x+dx, y: this.location.y+dy})
+		var corridor = this.getCurrentRoom().corridors[direction];
+		if (corridor && !corridor.obstacle){
+			this.location.x += dx;
+			this.location.y += dy;	
+		}
+	},
+	getDirection: function(from, to){
+		if (from.x == to.x){
+			if (from.y > to.y){
+				return 'north';
+			} else {
+				return 'south';
+			}
+		} else {
+			if (from.x > to.x){
+				return 'west';
+			} else {
+				return 'east';
+			}
+		}
 	}
 }
 
